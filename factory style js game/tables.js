97202e542cf8086f1389dfebb4478831a2b37324
Mecoolnotcool <106288499+Mecoolnotcool@users.noreply.github.com>
2025-06-04 15:12:14 -0500

    Create tables.js

---
+++ factory style js game/tables.js
@@ -0,0 +1,12 @@
+const recipes = {
+    sand_to_glass :{
+        intake: {
+           item: 'sand',
+           amount: 1
+        },
+        output: {
+           item: 'molten_glass',
+           amount: 1
+        }
+    }
+}
